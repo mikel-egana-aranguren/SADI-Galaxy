c34c15b2a8f5bc26af11b85acbd432b3955346e0
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2013-11-28 14:49:10 +0100

    use cases

--- TEST_SADI_services.sh
+++ TEST_SADI_services.sh
@@ -15,13 +15,13 @@
 
 # java -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://biordf.org:8080/cgi-bin/services/Dragon/getAlleleDescription.pl tests/getAlleleDescription_input_2.rdf > tests/getAlleleDescription_output_2.rdf
 
-# java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://sadiframework.org/examples/pdb2uniprot tests/pdb2uniprot-input.rdf > tests/pdb2uniprot-output.rdf
+# java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://sadiframework.org/examples/uniprot2pdb tests/pdb2uniprot-output.rdf > tests/uniprot2pdb-output.rdf
 
-java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://sadiframework.org/examples/uniprot2pdb tests/pdb2uniprot-output.rdf > tests/uniprot2pdb-output.rdf
+java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://sadiframework.org/examples/pdb2uniprot tests/pdb2uniprot-input-trimmed.rdf > tests/pdb2uniprot-output-trimmed.rdf
 
 # java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://sadiframework.org/examples/uniprotInfo tests/pdb2uniprot-output.rdf > tests/uniprotInfo-output.rdf
 
-java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://sadiframework.org/examples/uniprot2EntrezGene tests/uniprotInfo-output.rdf > tests/uniprot2EntrezGene-output.rdf
+# java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://sadiframework.org/examples/uniprot2EntrezGene tests/uniprotInfo-output.rdf > tests/uniprot2EntrezGene-output.rdf
 
-java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://dev.biordf.net/~kawas/cgi-bin/entrezGene2pubmed tests/uniprot2EntrezGene-output.rdf > tests/entrezGene2pubmed-ouput.rdf
+# java -Xmx2000M -Xms250M -jar galaxy-dist/tool-data/shared/jars/sadi_generic_client.jar http://dev.biordf.net/~kawas/cgi-bin/entrezGene2pubmed tests/uniprot2EntrezGene-output.rdf > tests/entrezGene2pubmed-ouput.rdf
 
